55d70b781baa3ef5ab162ed41fd97908b1f62fd9
Mithaniel Villard <mvillardhernandez@gaming.tech>
2025-01-18 17:15:19 +0100

    [ADD]
    Add tile prefab

--- Assets/Code/Scripts/Object/GrabbableObject.cs
+++ Assets/Code/Scripts/Object/GrabbableObject.cs
@@ -12,8 +12,6 @@ public class GrabbableObject : MonoBehaviour
     [field:SerializeField] public Vector2 minBound { get; set; }
     [field:SerializeField] public Vector2 maxBound { get; set; }
     
-    public static GrabbableObject CurrentObject;
-    
     private GameObject _shadow;
     private Vector3 _startScale;
     private SpriteRenderer _renderer;
@@ -21,28 +19,17 @@ public class GrabbableObject : MonoBehaviour
 
     private void Start()
     {
-        _renderer = GetComponent<SpriteRenderer>();
+        _renderer = GetComponentInChildren<SpriteRenderer>();
         _mat = _renderer.material;
-    }
-
-    void OnMouseEnter()
-    {
-        CurrentObject = this;
-        print("Hello");
-    }
-
-    private void OnMouseExit()
-    {
-        CurrentObject = null;
     }
 
     public void BeginDrag()
     {
         transform.DOScale(transform.localScale * 1.5f, 0.3f).SetEase(Ease.OutElastic);
-        _shadow = Instantiate(gameObject, transform, true);
+        
+        _shadow = Instantiate(_renderer.gameObject, transform, true);
         _shadow.transform.localPosition = new Vector3(shadowOffset.x, shadowOffset.y, 0f);
         _mat.SetInt("_Enabled", 1);
-        Destroy(_shadow.GetComponent<GrabbableObject>());
         if (_shadow.TryGetComponent(out SpriteRenderer renderer))
         {
             Material mat = new Material(Shader.Find("Sprites/Default"));    
